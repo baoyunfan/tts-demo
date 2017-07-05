a605000b8f7cbd2027b8ab382e1571fa02cf342e
yunfan <baoyunfan@gmail.com>
2017-07-05 01:10:14 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container" id="app">
+    <h1 class="text-primary">TTS Demo</h1>
+      <h5 class="text-muted">powered by <a href="https://vuejs.org/">Vue.js</a> and <a href="https://responsivevoice.org/">ResponsiveVoice.js</a></h5>
+      <div style="margin-top: 100px"></div>
+    <InputForm @inputFormSubmit="speak"></InputForm>
+  </div>
+</template>
+
+<script>
+require('./responsiveVoice.js')
+import InputForm from './components/InputForm'
+
+export default {
+  name: 'app',
+  components: {
+    InputForm
+  },
+  methods: {
+    speak: function (text) {
+      console.log(text)
+      responsiveVoice.speak(text)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row" id="inputForm">
+    <div class="col-md-10 col-md-offset-1">
+      <form id="speakForm" class="well form-inline" v-on:submit.prevent="inputFormSubmit">
+        <div class="row">
+          <div class="form-group col-md-10">
+            <label class="sr-only" for="formInput">Input</label>
+            <textarea id="textInput" class="form-control" placeholder="Enter text to read" v-model="textToRead"></textarea>
+          </div>
+          <div class="form-group col-md-2">
+            <input class="btn btn-primary" type='submit' value='🔊 Speak' />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  data () {
+    return {
+      textToRead: ''
+    }
+  },
+  methods: {
+    inputFormSubmit: function () {
+      this.$emit('inputFormSubmit', this.textToRead)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#textInput {
+  width: 100%;
+}
+#speakForm {
+  border-radius: 10px;
+  border: 1px #ccc solid;
+}
+</style>
