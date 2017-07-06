7e048650caa5896827844961a039b9ac8b2f8d60
yunfan <baoyunfan@gmail.com>
2017-07-06 02:09:45 -0400

    Added voice selection and stop button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="text-primary">TTS Demo</h1>
       <h5 class="text-muted">powered by <a href="https://vuejs.org/">Vue.js</a> and <a href="https://responsivevoice.org/">ResponsiveVoice.js</a></h5>
       <div style="margin-top: 100px"></div>
-    <InputForm @inputFormSubmit="speakIt"></InputForm>
+    <InputForm></InputForm>
   </div>
 </template>
 
@@ -15,11 +15,6 @@ export default {
   components: {
     InputForm
   },
-  methods: {
-    speakIt: function (text) {
-      responsiveVoice.speak(text)
-    }
-  }
 }
 </script>
 
